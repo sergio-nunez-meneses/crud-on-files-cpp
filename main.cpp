ba8dcd2b02d34dedd8911350d28b9b720c564666
sergionunezmeneses <snunezmeneses@gmail.com>
2022-03-30 10:58:09 +0200

    Get project path in function main in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
-//
-// Created by Sergio Nunez Meneses on 30/03/2022.
-//
+#include <iostream>
+#include <filesystem>
+
+using namespace std;
+
+namespace fs = __fs::filesystem;
+
+int main()
+{
+	string projectPath = fs::current_path().parent_path().string();
+	cout << projectPath << endl;
+}
 
