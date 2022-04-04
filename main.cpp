eed6f6be918a1f21f4d8b04b18e9683485d04fd5
sergionunezmeneses <snunezmeneses@gmail.com>
2022-04-04 13:21:31 +0200

    WIP Add user interaction to edit file after reading in function main in main.cpp

--- main.cpp
+++ main.cpp
@@ -21,15 +21,15 @@ int main()
 {
 	string cwd = getCurrentProjectPath();
 	string fileDirPath = cwd + "/created-files";
-	string filePath, fileContent;
+	string filePath, fileContent, fileName;
 	fstream file;
 
 	if (!fs::is_directory(fileDirPath))
 		fs::create_directory(fileDirPath);
 
 	cout << "Howdy, what would you like to do? Type a number and press enter:" << endl;
-	cout << "\t\t[1] Read a file." << endl;
-	cout << "\t\t[2] Create a text file." << endl;
+	cout << "\t\t[1] Read a file" << endl;
+	cout << "\t\t[2] Create a text file" << endl;
 
 	int action;
 	cin >> action;
@@ -39,19 +39,18 @@ int main()
 
 	if (action == 1)
 	{
+		cout << "\nWhich file would you like to read? Type a number and press enter:" << endl;
+
 		vector<string> files;
 
-		cout << "Which file would you like to read? Type a number and press enter:" << endl;
-
 		int fileIndex = 0;
-		for (const auto &entry : fs::directory_iterator(fileDirPath))
+		for (const auto &entry: fs::directory_iterator(fileDirPath))
 		{
 			files.push_back(entry.path().string());
 			++fileIndex;
 
 			cout << "\t\t[" << fileIndex << "] " << entry.path().string() << endl;
 		}
-
 		cin >> fileIndex;
 
 		filePath = files[fileIndex - 1];
@@ -59,11 +58,45 @@ int main()
 
 		if (file.is_open())
 		{
-			cout << "Opened file " << fs::path(filePath).filename().string() << endl;
+			fileName = fs::path(filePath).filename().string();
 
+			cout << "\nReading file: " << fileName << "\n" << endl;
 			while (getline(file, fileContent))
 				cout << fileContent << endl;
+
 			file.close();
+			cout << "\n*-- End reading file --*" << endl;
+
+			cout << "\nWould you like to edit this file?" << endl;
+			cout << "\t\t[1] Yes" << endl;
+			cout << "\t\t[2] No" << endl;
+
+			cin >> action;
+
+			if (action == 1)
+			{
+				file.open(filePath, ios::out | ios::app);
+
+				if (file.is_open())
+				{
+					cout << "\nEditing file: " << fileName << endl;
+					cout << "(Press enter to save and continue, type exit and press enter to stop)\n" << endl;
+
+					while (getline(cin, fileContent))
+					{
+						if (fileContent != "exit")
+							file << fileContent << endl;
+						else
+							break;
+					}
+					file.close();
+					cout << "\n*-- End editing file --*" << endl;
+				}
+				else
+					cout << "Unable to open file" << endl;
+			}
+			else
+				file.close();
 		}
 		else
 			cout << "Unable to open file" << endl;
