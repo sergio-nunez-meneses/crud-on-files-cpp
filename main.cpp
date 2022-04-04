b0102cfcae4650554fa1d09b8f678971537c902a
sergionunezmeneses <snunezmeneses@gmail.com>
2022-04-04 19:44:49 +0200

    Update code style in main.cpp

--- main.cpp
+++ main.cpp
@@ -16,9 +16,10 @@ vector<string> getStoredFiles(const string &path)
 {
 	vector<string> files;
 
-	for (const auto &entry: fs::directory_iterator(path))
-		files.push_back(entry.path().string());
-
+	for (const auto &entry: fs::directory_iterator(path)) {
+		string filePath = entry.path().string();
+		files.push_back(filePath);
+	}
 	return files;
 }
 
@@ -37,16 +38,14 @@ int main()
 	string cwd = getCurrentProjectPath();
 	string fileDirPath = cwd + "/created-files";
 
-	if (!fs::is_directory(fileDirPath))
-		fs::create_directory(fileDirPath);
+	if (!fs::is_directory(fileDirPath)) fs::create_directory(fileDirPath);
 
 	vector<string> files = getStoredFiles(fileDirPath);
 	string filePath, fileContent;
 	fstream file;
 
 	cout << "Howdy, what would you like to do? Type number and press enter:" << endl;
-	if (!files.empty())
-		cout << "\t\t[r] Read a file" << endl;
+	if (!files.empty()) cout << "\t\t[r] Read a file" << endl;
 	cout << "\t\t[c] Create a text file" << endl;
 
 	string action;
@@ -68,9 +67,7 @@ int main()
 			string fileName = fs::path(filePath).filename().string();
 
 			cout << "\nReading file: " << fileName << "\n" << endl;
-			while (getline(file, fileContent))
-				cout << fileContent << endl;
-
+			while (getline(file, fileContent)) cout << fileContent << endl;
 			file.close();
 			cout << "\n*-- End reading file --*" << endl;
 
@@ -91,20 +88,16 @@ int main()
 
 					while (getline(cin, fileContent))
 					{
-						if (fileContent != "exit")
-							file << fileContent << endl;
-						else
-							break;
+						if (fileContent != "exit") file << fileContent << endl;
+						else break;
 					}
 					file.close();
 					cout << "\n*-- End editing file --*" << endl;
 				}
 			}
-			else
-				file.close();
+			else file.close();
 		}
-		else
-			cout << "Unable to open file" << endl;
+		else cout << "Unable to open file" << endl;
 	}
 	else if (action == "c")
 	{
@@ -113,14 +106,12 @@ int main()
 		string fileName;
 		cin >> fileName;
 
-		if (fileName.find('.') == string::npos)
-			fileName += ".txt";
+		if (fileName.find('.') == string::npos) fileName += ".txt";
 		else
 		{
 			size_t needle = fileName.find_last_of('.'); // unsigned integer type of the result of the sizeof operator
 
-			if (fileName.substr(needle + 1) != "txt")
-				fileName = fileName.substr(0, needle) + ".txt";
+			if (fileName.substr(needle + 1) != "txt") fileName = fileName.substr(0, needle) + ".txt";
 		}
 
 		filePath = fileDirPath + "/" + fileName;
@@ -133,16 +124,13 @@ int main()
 
 			while (getline(cin, fileContent))
 			{
-				if (fileContent != "exit")
-					file << fileContent << endl;
-				else
-					break;
+				if (fileContent != "exit") file << fileContent << endl;
+				else break;
 			}
 			file.close();
 			cout << "\n*-- End editing file --*" << endl;
 		}
-		else
-			cout << "Unable to open file" << endl;
+		else cout << "Unable to open file" << endl;
 	}
 
 	return 0;
